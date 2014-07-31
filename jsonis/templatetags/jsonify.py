b23532844168afba7871636634e49dda0a19333d
Martin Putniorz <mputniorz@gmail.com>
2014-07-31 16:01:01 +0200

    Python3 template tag, biatch

--- jsonis/templatetags/jsonify.py
+++ jsonis/templatetags/jsonify.py
@@ -1,11 +1,16 @@
 from django.conf import settings
 from django.template import Library
 
-from ..utils import MyJSONEncoder
+from ..utils import MyJSONEncoder, PY3
 
 register = Library()
 
-encoder = MyJSONEncoder(ensure_ascii=False, encoding=settings.DEFAULT_CHARSET)
+if PY3:
+    encoder = MyJSONEncoder(ensure_ascii=False)
+else:
+    encoder = MyJSONEncoder(ensure_ascii=False,
+                            encoding=settings.DEFAULT_CHARSET)
+
 
 @register.filter
 def jsonify(obj):
